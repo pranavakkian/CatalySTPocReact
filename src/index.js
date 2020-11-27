394e56a3cba418e653ffd233f0d118eb1e62fdf2
eroshikjarvis <47566925+kishore0079@users.noreply.github.com>
2020-11-27 19:17:54 +0530

    Reconfigured

--- src/index.js
+++ src/index.js
@@ -6,5 +6,10 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render( < React.StrictMode > < App / > < /React.StrictMode>, document.getElementById('root'));
-        reportWebVitals();
+ReactDOM.render(
+  <React.StrictMode>
+    <App /> 
+  </React.StrictMode>, 
+  document.getElementById('root'));
+
+reportWebVitals();
