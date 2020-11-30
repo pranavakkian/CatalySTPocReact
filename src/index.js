ae3ac57eeb4603d67aa61ef7ee36789baaa7dab8
Kishore0079 <eroshiktony@gmail.com>
2020-11-30 18:39:50 +0530

    Created Redux Action

--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import Login from './Components/Login/index';
 import reportWebVitals from './reportWebVitals';
 import 'semantic-ui-css/semantic.min.css';
 
 ReactDOM.render(
-  <React.StrictMode />,
+  <React.StrictMode>
+    <Login />
+  </React.StrictMode>,
   document.getElementById('root'),
 );
 
