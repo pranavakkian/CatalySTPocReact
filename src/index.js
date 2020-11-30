752a5973b5cdd5798ab4421789ff32189f25fbdf
P-1-X-3-L <72730398+P-1-X-3-L@users.noreply.github.com>
2020-11-30 18:02:01 +0530

    Poc 6 (#7)
    * Created Login Page Component
    
    * Created LoginPage Component
    
    * Created LoginPage Component
    
    * removed Snaphot
    
    * Created Login Page Component

--- src/index.js
+++ src/index.js
@@ -3,13 +3,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'semantic-ui-css/semantic.min.css';
+import Login from './Components/Login';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Login />
   </React.StrictMode>,
   document.getElementById('root'),
 );
