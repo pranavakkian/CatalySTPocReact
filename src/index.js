2438c751bc852b487c22afee6148526a83bff092
P-1-X-3-L <72730398+P-1-X-3-L@users.noreply.github.com>
2020-12-01 13:26:10 +0530

    Installed React-Router-Dom and used BrowserRouter (#10)
    * Installed React-Router-Dom and used BrowserRouter
    
    * Installed React-Router-Dom and added BrowserRouter

--- src/index.js
+++ src/index.js
@@ -3,13 +3,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { BrowserRouter } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
 import 'semantic-ui-css/semantic.min.css';
 import Login from './Components/Login';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Login />
+    <BrowserRouter>
+      <Login />
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root'),
 );
