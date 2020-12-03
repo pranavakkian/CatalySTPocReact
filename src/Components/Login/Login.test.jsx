770ac49e867d5de994a49fe91f947de7a2b2cf3c
P-1-X-3-L <72730398+P-1-X-3-L@users.noreply.github.com>
2020-12-03 14:21:00 +0530

    Made Changes to Login.test.js

--- src/Components/Login/Login.test.jsx
+++ src/Components/Login/Login.test.jsx
@@ -7,41 +7,46 @@ import Store from '../../Store';
 
 describe('Login', () => {
   const MockStore = configureMockStore();
-  const store = Store;
+  const mockstore = MockStore(() => ({
+    UserInfoReducer: {
+      isLoggedIn: true,
+      userName: 'test',
+    },
+  }));
+  const mockstore2 = MockStore(() => ({
+    UserInfoReducer: {
+      isLoggedIn: false,
+      userName: null,
+    },
+  }));
   test('Component rendered successfully', () => {
-    const mockstore = MockStore(() => ({
-      UserInfoReducer: {
-        isLoggedIn: true,
-        userName: 'test',
-      },
-    }));
     const { container } = render(<Provider store={mockstore}><Login /></Provider>);
     expect(container).toMatchSnapshot();
   });
   test('Login Failed', () => {
     // userEvent.type(screen.getAllByRole('textbox')[0], '');
     // userEvent.type(screen.getAllByRole('textbox')[1], '');
-    render(<Provider store={store}><Login /></Provider>);
+    render(<Provider store={mockstore2}><Login /></Provider>);
     userEvent.click(screen.getByRole('button', { name: 'Submit' }));
     expect(screen.getByText('Invalid Credentials.')).toBeInTheDocument();
     userEvent.click(screen.getByRole('button', { name: 'OK' }));
-    const { UserInfoReducer } = store.getState();
+    const { UserInfoReducer } = mockstore2.getState();
     expect(UserInfoReducer).toStrictEqual({ isLoggedIn: false, userName: null });
   });
   test('Login Successful', () => {
-    render(<Provider store={store}><Login /></Provider>);
-    userEvent.type(screen.getAllByRole('textbox')[0], 'admin');
+    render(<Provider store={mockstore}><Login /></Provider>);
+    userEvent.type(screen.getAllByRole('textbox')[0], 'test');
     userEvent.type(screen.getAllByRole('textbox')[1], 'admin123');
     userEvent.click(screen.getByRole('button', { name: 'Submit' }));
-    const { UserInfoReducer } = store.getState();
-    expect(UserInfoReducer).toStrictEqual({ isLoggedIn: true, userName: 'admin' });
+    const { UserInfoReducer } = mockstore.getState();
+    expect(UserInfoReducer).toStrictEqual({ isLoggedIn: true, userName: 'test' });
   });
   test('Login Successful', () => {
-    render(<Provider store={store}><Login /></Provider>);
+    render(<Provider store={mockstore}><Login /></Provider>);
     userEvent.type(screen.getAllByRole('textbox')[0], 'abc');
     userEvent.type(screen.getAllByRole('textbox')[1], 'abc123');
     userEvent.click(screen.getByRole('button', { name: 'Submit' }));
-    const { UserInfoReducer } = store.getState();
-    expect(UserInfoReducer).toStrictEqual({ isLoggedIn: true, userName: 'abc' });
+    const { UserInfoReducer } = mockstore.getState();
+    expect(UserInfoReducer).toStrictEqual({ isLoggedIn: true, userName: 'test' });
   });
 });
