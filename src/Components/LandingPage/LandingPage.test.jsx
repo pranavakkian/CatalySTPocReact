84458f13a00309f90743313b4f34d2627e446639
eroshikjarvis <47566925+kishore0079@users.noreply.github.com>
2020-12-03 20:28:09 +0530

    Signout Functionality in LandingPage (#19)
    * Create LandingPage Component
    
    * Created Landing Page Container and Store was Provided
    
    * Created LandingPage Container and store was provided
    
    * Created Landing Page Container and Store was Provided
    
    * Created Landing Page Container and Store was Provided
    
    * Created Landing Page Container and Store was Provided
    
    * Signout Functionality in LandingPage
    
    * Signout functionality in LandingPage

--- src/Components/LandingPage/LandingPage.test.jsx
+++ src/Components/LandingPage/LandingPage.test.jsx
@@ -1,10 +1,12 @@
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import { Provider } from 'react-redux';
 import configureMockStore from 'redux-mock-store';
 import LandingPage from './index';
 
 describe('LandingPage', () => {
   const mockStore = configureMockStore();
+
   test('Component rendered successfully', () => {
     const store = mockStore(() => ({
       UserInfoReducer: {
@@ -14,5 +16,20 @@ describe('LandingPage', () => {
     }));
     const { container } = render(<Provider store={store}><LandingPage /></Provider>);
     expect(container).toMatchSnapshot();
+  });
+  test('Submit Button Working Properly', () => {
+    const store = mockStore(() => ({
+      UserInfoReducer: {
+        isLoggedIn: true,
+        userName: 'test',
+      },
+    }));
+    render(<Provider store={store}><LandingPage /></Provider>);
+    userEvent.click(screen.getByRole('button', { name: 'Sign-out' }));
+    const expectedAction = [
+      { payload: false, type: 'IS_LOGGED_IN' },
+      { payload: null, type: 'SET_USER_NAME' },
+    ];
+    expect(store.getActions()).toStrictEqual(expectedAction);
   });
 });
