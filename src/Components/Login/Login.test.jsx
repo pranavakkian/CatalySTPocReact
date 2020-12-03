521d2850f4613bce801128e67d8b76c35fa526c4
P-1-X-3-L <72730398+P-1-X-3-L@users.noreply.github.com>
2020-12-03 17:40:08 +0530

    POC-13 (#17)
    * Connected LoginForm to Store
    
    * poc-13
    
    * Linked Login Page to Store and Validation
    
    * Made Changes to Login.test.js
    
    * POC-13

--- src/Components/Login/Login.test.jsx
+++ src/Components/Login/Login.test.jsx
@@ -1,9 +1,66 @@
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import userEvent from '@testing-library/user-event';
+import configureMockStore from 'redux-mock-store';
 import Login from './index';
 
 describe('Login', () => {
+  const mockStore = configureMockStore();
   test('Component rendered successfully', () => {
-    const { container } = render(<Login />);
+    const store = mockStore(() => ({
+      UserInfoReducer: {
+        isLoggedIn: false,
+        userName: null,
+      },
+    }));
+    const { container } = render(<Provider store={store}><Login /></Provider>);
     expect(container).toMatchSnapshot();
+  });
+  test('Login Failed', () => {
+    const store = mockStore(() => ({
+      UserInfoReducer: {
+        isLoggedIn: false,
+        userName: null,
+      },
+    }));
+    render(<Provider store={store}><Login /></Provider>);
+    userEvent.click(screen.getByRole('button', { name: 'Submit' }));
+    expect(screen.getByText('Invalid Credentials.')).toBeInTheDocument();
+    userEvent.click(screen.getByRole('button', { name: 'OK' }));
+    expect(store.getActions()).toStrictEqual([]);
+  });
+  test('Login Successful for User-Admin', () => {
+    const store = mockStore(() => ({
+      UserInfoReducer: {
+        isLoggedIn: false,
+        userName: null,
+      },
+    }));
+    render(<Provider store={store}><Login /></Provider>);
+    userEvent.type(screen.getAllByRole('textbox')[0], 'admin');
+    userEvent.type(screen.getAllByRole('textbox')[1], 'admin123');
+    userEvent.click(screen.getByRole('button', { name: 'Submit' }));
+    const expectedAction = [
+      { payload: true, type: 'IS_LOGGED_IN' },
+      { payload: 'admin', type: 'SET_USER_NAME' },
+    ];
+    expect(store.getActions()).toStrictEqual(expectedAction);
+  });
+  test('Login Successful for User-abc', () => {
+    const store = mockStore(() => ({
+      UserInfoReducer: {
+        isLoggedIn: false,
+        userName: null,
+      },
+    }));
+    render(<Provider store={store}><Login /></Provider>);
+    userEvent.type(screen.getAllByRole('textbox')[0], 'abc');
+    userEvent.type(screen.getAllByRole('textbox')[1], 'abc123');
+    userEvent.click(screen.getByRole('button', { name: 'Submit' }));
+    const expectedAction = [
+      { payload: true, type: 'IS_LOGGED_IN' },
+      { payload: 'abc', type: 'SET_USER_NAME' },
+    ];
+    expect(store.getActions()).toStrictEqual(expectedAction);
   });
 });
--- src/Components/Login/LoginForm/Loginform.test.jsx
+++ src/Components/Login/LoginForm/Loginform.test.jsx
@@ -3,20 +3,35 @@ import userEvent from '@testing-library/user-event';
 import LoginForm from '.';
 
 describe('LoginForm', () => {
+  const mock = jest.fn();
   test('Component rendered successfully', () => {
-    const { container } = render(<LoginForm />);
+    const { container } = render(<LoginForm handleLoginForm={mock} />);
     expect(container).toMatchSnapshot();
   });
   test('handles username change correctly', () => {
-    render(<LoginForm />);
+    render(<LoginForm handleLoginForm={mock} />);
     expect(screen.getAllByRole('textbox')[0]).toHaveValue('');
     userEvent.type(screen.getAllByRole('textbox')[0], 'abc123');
     expect(screen.getAllByRole('textbox')[0]).toHaveValue('abc123');
   });
   test('handles password change correctly', () => {
-    render(<LoginForm />);
+    render(<LoginForm handleLoginForm={mock} />);
     expect(screen.getAllByRole('textbox')[1]).toHaveValue('');
     userEvent.type(screen.getAllByRole('textbox')[1], 'abc123');
     expect(screen.getAllByRole('textbox')[1]).toHaveValue('abc123');
+  });
+  test('Submit Button Working Properly', () => {
+    render(<LoginForm handleLoginForm={mock} />);
+    userEvent.type(screen.getAllByRole('textbox')[0], 'admin');
+    userEvent.type(screen.getAllByRole('textbox')[1], 'admin');
+    userEvent.click(screen.getByRole('button', { name: 'Submit' }));
+    expect(mock).toHaveBeenNthCalledWith(1, 'admin', 'admin');
+  });
+  test('Submit Button Working Properly', () => {
+    render(<LoginForm handleLoginForm={mock} />);
+    userEvent.type(screen.getAllByRole('textbox')[0], 'abc');
+    userEvent.type(screen.getAllByRole('textbox')[1], 'abc123');
+    userEvent.click(screen.getByRole('button', { name: 'Submit' }));
+    expect(mock).toHaveBeenNthCalledWith(1, 'abc', 'abc123');
   });
 });
