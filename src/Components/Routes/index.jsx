52b257da4141c597dc402182fd6a4d3cf712ab97
eroshikjarvis <47566925+kishore0079@users.noreply.github.com>
2020-12-04 10:28:18 +0530

    Created AddColleague Container and Routing from LandingPage (#22)
    * Create LandingPage Component
    
    * Created Landing Page Container and Store was Provided
    
    * Created LandingPage Container and store was provided
    
    * Created Landing Page Container and Store was Provided
    
    * Created Landing Page Container and Store was Provided
    
    * Created Landing Page Container and Store was Provided
    
    * Signout Functionality in LandingPage
    
    * Signout functionality in LandingPage
    
    * Created AddColleague Conatiner and routing
    
    * Created AddColleague Container and Route
    
    * Created AddColleague Container and Routing from LandingPage
    
    * Created AddColleague Container and Routing from LandingPage

--- src/Components/Routes/index.jsx
+++ src/Components/Routes/index.jsx
@@ -2,13 +2,15 @@ import { useSelector } from 'react-redux';
 import { Route, Switch } from 'react-router-dom';
 import LandingPage from '../LandingPage/index';
 import Login from '../Login';
-import RootPath from '../../Constants';
+import { RootPath, addColleaguePath } from '../../Constants';
+import AddColleaguePage from '../AddColleague';
 
 const Routes = () => {
   const isLoggedIn = useSelector((state) => state.UserInfoReducer.isLoggedIn);
   return isLoggedIn ? (
     <Switch>
       <Route exact path={RootPath} component={LandingPage} />
+      <Route path={addColleaguePath} component={AddColleaguePage} />
     </Switch>
   ) : (
     <Switch>
