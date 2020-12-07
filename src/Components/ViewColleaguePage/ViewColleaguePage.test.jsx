03d808bd98693bb5535ed38a8a676ac70f78b2a2
P-1-X-3-L <72730398+P-1-X-3-L@users.noreply.github.com>
2020-12-07 21:40:23 +0530

    ViewColleaguePage: Viewing Colleague Data

--- src/Components/ViewColleaguePage/ViewColleaguePage.test.jsx
+++ src/Components/ViewColleaguePage/ViewColleaguePage.test.jsx
@@ -1,9 +1,32 @@
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import configureMockStore from 'redux-mock-store';
 import ViewColleague from './index';
 
 describe('ViewColleaguePage', () => {
-  test('Rendered View Colleague Page Successfully', () => {
-    const { container } = render(<ViewColleague />);
+  const mockStore = configureMockStore();
+  test('Rendered ViewColleaguePageSuccessfully', () => {
+    const store = mockStore(() => ({
+      ColleagueReducer: {
+        data: [
+          {
+            name: 'Abc',
+            jobTitle: 'Engineer',
+            contact: '12345',
+          },
+        ],
+      },
+    }));
+    const { container } = render(<Provider store={store}><ViewColleague /></Provider>);
     expect(container).toMatchSnapshot();
+  });
+  test('Checking for no data in store', () => {
+    const store = mockStore(() => ({
+      ColleagueReducer: {
+        data: [],
+      },
+    }));
+    render(<Provider store={store}><ViewColleague /></Provider>);
+    expect(screen.getByText('Data Not Available')).toBeInTheDocument();
   });
 });
