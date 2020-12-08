8466f922855d135dac912864562eb8794a64774a
P-1-X-3-L <72730398+P-1-X-3-L@users.noreply.github.com>
2020-12-08 14:37:48 +0530

    LoginForm Validation

--- src/Components/Login/LoginForm/index.jsx
+++ src/Components/Login/LoginForm/index.jsx
@@ -6,16 +6,21 @@ const LoginForm = (props) => {
   const { handleLoginForm } = props;
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const handleSubmit = () => {
+    if (username !== '' && password !== '') {
+      handleLoginForm(username, password);
+    }
+  };
   return (
     <div>
       <Form>
+        <Form.Input>
+          <input required id="username" placeholder="Username" label="Username" value={username} onChange={(e) => setUsername(e.target.value)} />
+        </Form.Input>
         <Form.Field>
-          <input id="username" placeholder="Username" label="Username" value={username} onChange={(e) => setUsername(e.target.value)} />
+          <input required placeholder="Password" value={password} label="Password" type="password" onChange={(e) => setPassword(e.target.value)} />
         </Form.Field>
-        <Form.Field>
-          <input placeholder="Password" value={password} label="Password" type="password" onChange={(e) => setPassword(e.target.value)} />
-        </Form.Field>
-        <Button type="submit" onClick={() => handleLoginForm(username, password)}>Submit</Button>
+        <Button type="submit" onClick={handleSubmit}>Submit</Button>
       </Form>
     </div>
   );
